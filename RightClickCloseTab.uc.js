18783b0c06a7ac92bc1202c5b8f5290e0847f39a
荒并 <60814570+Koitoouav@users.noreply.github.com>
2026-02-14 20:21:29 +0800

    Add files via upload

---
+++ RightClickCloseTab.uc.js
@@ -0,0 +1,34 @@
+console.log("RightClickCloseTab.js");
+
+(() => {
+    if (gBrowser===undefined)
+        UC_API.Runtime.startupFinished().then(start_RightClickCloseTab);
+    else
+        start_RightClickCloseTab();
+
+    function start_RightClickCloseTab() {
+        gBrowser.tabContainer.addEventListener("TabOpen", eventTabAdded, false);
+        function eventTabAdded(event) {
+            var tab = event.target;
+            tab.addEventListener('click', onTabEvent);
+            tab.addEventListener('contextmenu', onTabEvent);
+        }
+
+
+        function onTabEvent(event) {
+            if (event.button != 2 || event.shiftKey  )
+                return;
+
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (event.type == 'click')
+                gBrowser.removeTab(this, {animate: true});
+        }
+
+        gBrowser.tabContainer.querySelectorAll('tab').forEach( function(tab, index) {
+            tab.addEventListener('click', onTabEvent);
+            tab.addEventListener('contextmenu', onTabEvent);
+        });
+    }
+})();
